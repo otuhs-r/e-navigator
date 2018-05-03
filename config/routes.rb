895feaea7caf9c463cab0a7ba00ce4910631307d
enimi_chan <yekcim.114@gmail.com>
2018-05-03 17:07:03 +0900

    feat: signup & login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get '/show', to: 'pages#show'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
 end
