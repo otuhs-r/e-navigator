4f3180eadbcac2e636840881640f7f64f5b636c7
enimi_chan <yekcim.114@gmail.com>
2018-05-09 16:15:26 +0900

    refactor: modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#index'
-  get '/show', to: 'pages#show'
+  root 'users#index'
   devise_for :users, :controllers => { :registrations => :registrations }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users/me', to: 'users#show'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: '/letter_opener'
