8b57798c009f5ec36f8f2da55c60466881ecda3a
enimi_chan <yekcim.114@gmail.com>
2018-05-14 19:40:13 +0900

    refactor: use default action allocation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def index
   end
 
-  def show
+  def me
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
   devise_for :users, :controllers => { :registrations => :registrations }
-  get '/users/me', to: 'users#show'
+  get '/users/me'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: '/letter_opener'
