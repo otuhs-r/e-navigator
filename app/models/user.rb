e990b7643eafb4a872eff8d2b63f8584f6357032
enimi_chan <yekcim.114@gmail.com>
2018-05-24 22:06:16 +0900

    feat: show users other than oneself

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   enum gender: { male: 0, female: 1 }
   has_many :interviews, dependent: :destroy
+
+  def age
+    (Date.today.to_s(:number).to_i - self.birth_day.to_s(:number).to_i) / 10000
+  end
+
+  def interview_datetime
+  end
 end
